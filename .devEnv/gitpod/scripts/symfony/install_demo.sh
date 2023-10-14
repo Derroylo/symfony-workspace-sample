b832d5c1b63ece5f975bc1a2beac108eb79cbcf0
Derroylo <72140754+Derroylo@users.noreply.github.com>
2023-10-15 01:15:50 +0200

    Fix install_demo script

--- .devEnv/gitpod/scripts/symfony/install_demo.sh
+++ .devEnv/gitpod/scripts/symfony/install_demo.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Infos for GitpodTool
+#
+# gptBranch: symfony
+# gptBranchDescription: Commands for symfony
+# gptCommand: install_demo
+# gptDescription: Install the symfony demo
+
 symfony new demo --demo
 rm -rf demo/.git
 rm -rf demo/README.md
